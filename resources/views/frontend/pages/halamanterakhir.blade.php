6a679db45e3f89ef6277eb4382c08d8b9cf4f396
Maristo-10 <maristositoruz@gmail.com>
2025-02-22 22:22:03 +0700

    update logo

--- resources/views/frontend/pages/halamanterakhir.blade.php
+++ resources/views/frontend/pages/halamanterakhir.blade.php
@@ -7,8 +7,8 @@
             <div class="row  mt-0" style="place-content: center">
                 <div class="col-12 col-lg-8 mb-5 text-center position-relative">
                     <div class="row">
-                        <div class="col-12 col-lg-12 mb-2 text-center position-relative" style="border-radius: 12px">
-                            <a class="navbar-brand" href="/"><img src="{{ asset('/frontend/assets/images/logo2.png') }}"
+                        <div class="col-12 col-lg-12 mb-4 text-center position-relative" style="border-radius: 12px">
+                            <a class="navbar-brand" href="/"><img src="{{ asset('/frontend/assets/images/logo4.jpg') }}"
                                     alt="Brand Logo" title="Brand Logo" class="img-fluid" style="height: 300px"></a>
                         </div>
                         <div class="col-12 col-lg-12 text-center bg-white position-relative" style="border-radius: 12px">
